fb143b8603cd5b26dc39709505e87866f5242d8c
Dominik Moritz <domoritz@gmail.com>
2013-03-30 20:57:54 +0100

    Add profiles

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import argparse
 import sys
 import multiprocessing
@@ -11,9 +13,11 @@ import sosat.preprocessing as preprocessing
 
 VERBOSE = False
 
+
 class MyProcess(Process):
     def run(self):
         Process.run(self)
+
 
 def print_solution(instance_solution, num_vars):
     instance, p_solution = instance_solution
@@ -47,11 +51,10 @@ if __name__ == '__main__':
                      default=1, type=int,
                      help='number of processes')
     clp.add_argument('-f', '--factor', dest='f',
-                     default=0, type=int, 
+                     default=1, type=int,
                      help='number of factored (most-constrained) variables')
     clp.add_argument('infile', nargs='?', type=argparse.FileType('r'),
                      default=sys.stdin)
-
 
     args = clp.parse_args()
     VERBOSE = args.verbose
@@ -59,37 +62,36 @@ if __name__ == '__main__':
     num_vars, clauses = parser.parse(args.infile)
     factored_instances = preprocessing.factored_instances(num_vars, clauses, min(args.f, num_vars))
 
-    if args.verbose:
-        print "Reduced instances:"
-        
-        for i in factored_instances:
-            print i, " / ", num_vars, "original variables"
-     
-    def start(instance, seed, queue):
-        if instance == False:
+    if args.algo == 'genetic':
+        algo = ga.GeneticAlgorithm
+    elif args.algo == 'ant':
+        algo = aa.AntColonyAlgorithm
+    else:
+        print("No such algorithm.")
+
+    def dprint(*args, **kwargs):
+        if VERBOSE:
+            __builtins__.print(*args, **kwargs)
+
+    dprint("c Reduced instances:")
+
+    for i in factored_instances:
+        dprint("c", i, " / ", num_vars, "original variables")
+
+    def start(instance, config, queue):
+        if not instance == False:
             # detected that this is unsolvable during preprocessing
             queue.put((instance, False))
             return
-        
+
         if len(instance[1]) == 0:
             # found solution during preprocessing
             queue.put((instance, []))
             return
 
-        options = {
-            'VERBOSE': args.verbose,
-            'SEED': seed
-        }
+        dprint("c Start one process with config", config)
 
-        if args.verbose:
-            print "c Start one process with args", options
-
-        if args.algo == 'genetic':
-            a = ga.GeneticAlgorithm(instance[0], instance[1], options)
-        elif args.algo == 'ant':
-            a = aa.AntColonyAlgorithm(instance[0], instance[1], options)
-        else:
-            print "No such algorithm."
+        a = algo(instance[0], instance[1], config)
 
         solution = a.run()
         queue.put((instance, solution))
@@ -99,18 +101,36 @@ if __name__ == '__main__':
 
     if not args.N:
         args.N = multiprocessing.cpu_count()
- 
-    seeds = range(args.seed, args.seed + args.N)
+
+    # run profiles
+    dprint("c Run {} profiles".format(len(algo.profiles)))
+
+    for profile in algo.profiles:
+        config = {
+            'VERBOSE': args.verbose,
+            'SEED': args.seed
+        }
+        config.update(profile)
+        for instance in factored_instances:
+            p = MyProcess(target=start, args=(instance, config, queue))
+            p.start()
+            processes.append(p)
+
+    # run default config with seed
+    seeds = range(args.seed, args.seed + args.N - max(0, len(algo.profiles)))
     for seed in seeds:
         for instance in factored_instances:
-            p = MyProcess(target=start, args=(instance, seed, queue))
+            config = {
+                'VERBOSE': args.verbose,
+                'SEED': seed + len(algo.profiles)
+            }
+            p = MyProcess(target=start, args=(instance, config, queue))
             p.start()
             processes.append(p)
 
     false_counter = 0
 
-    if VERBOSE:
-        print "Waiting for at most", len(seeds) * len(factored_instances), "results..."
+    dprint("c Waiting for at most", len(seeds) * len(factored_instances), "results...")
 
     for i in xrange(len(seeds) * len(factored_instances)):
         solution = queue.get()
@@ -118,12 +138,12 @@ if __name__ == '__main__':
             false_counter += 1
         elif solution is not None:
             print_solution(solution, num_vars)
-     
+
             for process in processes:
                 process.terminate()
-    
+
             exit()
-    
+
     if false_counter == len(seeds) * len(factored_instances):
         print_solution(False, 0)
     else:
