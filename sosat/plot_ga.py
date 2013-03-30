9ee29548fec41dd821b3509c2abe10eef1c5c242
Dominik Moritz <domoritz@gmail.com>
2013-03-30 17:30:27 +0100

    Add variable mutation rate

--- sosat/plot_ga.py
+++ sosat/plot_ga.py
@@ -7,16 +7,18 @@ import numpy as np
 
 def run():
     algo = ga.GeneticAlgorithm
-    f = open('instances/random_ksat13.dimacs')
+    f = open('instances/random_ksat12.dimacs')
     num_vars, clauses = parser.parse(f)
 
     conf = {
         'COLLECT_STATS': True,
-        'MAX_ITERATIONS': 1000
+        'MAX_ITERATIONS': 600
     }
 
     a = algo(num_vars, clauses, conf)
     a.run()
+
+    f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
 
     xs = []
     ys = []
@@ -24,38 +26,72 @@ def run():
         for v in d['values']:
             xs.append(k)
             ys.append(v)
-    plt.scatter(xs, ys, s=10, alpha=0.15)
-    '''
-    # or hist2d
-    import matplotlib.cm as cm
-    plt.hexbin(xs, ys, gridsize=45, cmap=cm.jet, bins=None)
-    plt.axis([min(xs), max(xs), min(ys), max(ys)])
 
-    cb = plt.colorbar()
-    cb.set_label('Number of clauses')'''
+    import matplotlib.cm as cm
+    from matplotlib.colors import LogNorm
+    x_range = max(xs) - min(xs)
+    y_range = max(ys) - min(ys)
+
+    #ax1.scatter(xs, ys, s=10, alpha=0.15)
+
+    #'''
+    h = ax1.hist2d(xs, ys, bins=(x_range, y_range), norm=LogNorm())
+    f.subplots_adjust(right=0.8)
+    #f.colorbar(h[3], ax=ax1)
+    #'''
+    '''
+    ax1.hexbin(xs, ys, gridsize=(x_range, y_range), cmap=cm.jet, bins=None)
+    ax1.axis([min(xs), max(xs), min(ys), max(ys)])
+
+    #cb = plt.colorbar()
+    #cb.set_label('Number of clauses')
+    #'''
 
     data = np.array([x['best'] for x in a.progress])
-
     xs = range(len(data))
     ys = data
     #ys = np.abs(data - len(clauses))
     #plt.ylim([0, np.max(data)])
     plt.xlim([0, len(data)])
-
     #plt.semilogy(xs, ys, 'bo-', label='Performance')
-    plt.plot(xs, ys, 'ro-', label='Performance')
+    ax1.plot(xs, ys, 'ro-', label='Best performance')
+
+    # average
+    data = np.array([x['values'] for x in a.progress])
+    xs = range(len(data))
+    ys = np.average(data, axis=1)
+    plt.xlim([0, len(data)])
+    ax1.plot(xs, ys, 'go-', label='Average performance')
+
+    # std
+    data = np.array([x['values'] for x in a.progress])
+    xs = range(len(data))
+    ys = np.std(data, axis=1)
+    ax2.plot(xs, ys, 'b-', label='Standard derivative')
+
+    # mr
+    data = np.array([x['mr'] for x in a.progress])
+    xs = range(len(data))
+    ys = data
+    ax3.plot(xs, ys, 'g-', label='Mutation rate')
 
     # Styling
+    f.subplots_adjust(hspace=0)
     plt.xlabel('Iteration')
-    plt.ylabel('Satisfied clauses')
-    plt.title('Genetic Algorithm - Performance')
-    plt.grid(True, which="both", linestyle="dotted")
+    ax1.set_ylabel('Satisfied clauses')
+    ax2.set_ylabel('Value')
+    ax1.set_title('Genetic Algorithm - Performance')
+    ax1.grid(True, which="both", linestyle="dotted")
+    ax1.legend()
+    ax2.legend()
+    ax3.legend()
 
     from matplotlib.backends.backend_pdf import PdfPages
     pp = PdfPages('plots/ga_performance_ksat13.pdf')
     plt.savefig(pp, format='pdf')
     pp.close()
 
+    print "Plotted"
     plt.show()
 
 if __name__ == '__main__':
